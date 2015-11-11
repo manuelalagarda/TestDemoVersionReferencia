69569537017e2a1db7eb4f78e40221a33c14a4a9
Manuel Alagarda <manuel.alagarda@nunsys.com>
2015-11-11 16:55:47 +0100

    Versión de referencia

---
+++ TestDemo/AdventureWorks2008R2.cs
@@ -0,0 +1,53 @@
+namespace TestDemo
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class AdventureWorks2008R2 : DbContext
+    {
+        public AdventureWorks2008R2()
+            : base("name=AdventureWorks2008R2")
+        {
+        }
+
+        public virtual DbSet<SalesOrderDetail> SalesOrderDetails { get; set; }
+        public virtual DbSet<SalesOrderHeader> SalesOrderHeaders { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SalesOrderDetail>()
+                .Property(e => e.UnitPrice)
+                .HasPrecision(19, 4);
+
+            modelBuilder.Entity<SalesOrderDetail>()
+                .Property(e => e.UnitPriceDiscount)
+                .HasPrecision(19, 4);
+
+            modelBuilder.Entity<SalesOrderDetail>()
+                .Property(e => e.LineTotal)
+                .HasPrecision(38, 6);
+
+            modelBuilder.Entity<SalesOrderHeader>()
+                .Property(e => e.CreditCardApprovalCode)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<SalesOrderHeader>()
+                .Property(e => e.SubTotal)
+                .HasPrecision(19, 4);
+
+            modelBuilder.Entity<SalesOrderHeader>()
+                .Property(e => e.TaxAmt)
+                .HasPrecision(19, 4);
+
+            modelBuilder.Entity<SalesOrderHeader>()
+                .Property(e => e.Freight)
+                .HasPrecision(19, 4);
+
+            modelBuilder.Entity<SalesOrderHeader>()
+                .Property(e => e.TotalDue)
+                .HasPrecision(19, 4);
+        }
+    }
+}
---
+++ TestDemo/ProcesoDemo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestDemo
+{
+    public class ProcesoDemo
+    {
+        private AdventureWorks2008R2 context = new AdventureWorks2008R2();
+
+        public void Ejecutar()
+        {
+            ActualizarPreciosDeLasLineasDeDetalle();
+            EliminarUltimaLineaDeDetalle();
+            AnyadirNuevaLineaDeDetalle();
+            GuardarCambios();            
+        }
+
+        private void ActualizarPreciosDeLasLineasDeDetalle()
+        {
+            var salesOrderDetails = context.SalesOrderDetails.Where(o => o.SalesOrderDetailID <= 1000);
+            decimal increase = (decimal)1.10;
+            foreach (var salesOrderDetail in salesOrderDetails)
+            {
+                salesOrderDetail.UnitPrice = salesOrderDetail.UnitPrice * increase;
+            }
+        }
+
+        private void AnyadirNuevaLineaDeDetalle()
+        {
+            var salesOrderDetails = context.SalesOrderDetails;
+
+            SalesOrderDetail newDetail = new SalesOrderDetail();
+            newDetail.SalesOrderID = 75123;
+            newDetail.OrderQty = 2;
+            newDetail.ProductID = 712;
+            newDetail.SpecialOfferID = 1;
+            newDetail.UnitPrice = 1;
+            newDetail.UnitPriceDiscount = 0;
+            newDetail.ModifiedDate = DateTime.Now;
+
+            salesOrderDetails.Add(newDetail);
+
+        }
+
+        private void EliminarUltimaLineaDeDetalle()
+        {
+            var salesOrderDetails = context.SalesOrderDetails;
+            SalesOrderDetail lastDetail = salesOrderDetails.OrderByDescending(p => p.SalesOrderDetailID).FirstOrDefault();
+            salesOrderDetails.Remove(lastDetail); 
+        }
+
+        private void GuardarCambios()
+        {
+            context.SaveChanges();
+        }
+
+    }
+}
